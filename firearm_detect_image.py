560c37b3066f3a7dddb4ae8808e92bf2c8a7f6b1
Bruno Teles <45904436+dbrunoteles@users.noreply.github.com>
2021-06-27 13:59:00 -0300

    Add files via upload

---
+++ firearm_detect_image.py
@@ -0,0 +1,38 @@
+import torch
+import cv2
+import time
+from my_lib import *
+
+if __name__ == "__main__":
+
+    # Creating a dictionary to save each target object's ID ({id: center of object's bbox})
+    id_boxes = {}
+
+    # Start ID count
+    id_count = 1
+
+    # Code execution start time
+    start = time.time()
+
+    # Build model
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='custom_yolov5s.pt')
+
+    # Setting target object name for text variables
+    target_object = 'Firearms at this moment'
+
+    # Import image
+    img = cv2.imread('shooting.jpg')
+
+    # Setting frame number for the function (Image = 1 frame)
+    frame_number = 1
+
+    # Detect humans on the image
+    frame, id_count = detect_and_track(img, model, frame_number, id_boxes, id_count, target_object)
+
+    # Show image on screen
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+
+    # Code execution end time
+    end = time.time()
+    print(f'Time taken to run the code: %.3f seconds' % (end-start))
